5e9aa33c562ce3545e564a8924de3d7ceb601c6c
Rhuann01 <rhuann2006@gmail.com>
2026-07-01 22:30:13 -0300

    chore: add shadcn

--- src/components/OnBoarding.tsx
+++ src/components/OnBoarding.tsx
@@ -2,15 +2,24 @@ import { LoadingScreen } from "./ui/LoadingScreen";
 
 export const OnBoarding = () => {
   return (
-    <section className="h-screen w-screen flex flex-col items-center justify-end gap-2 text-4xl text-white font-semibold">
+    <section className="h-screen w-screen flex flex-col items-center justify-end gap-2 font-semibold">
       {/*      <LoadingScreen />       */}
 
       <img
         src="/BackDrop.png"
         className=" absolute top-0 left-0 w-screen h-screen -z-1"
       />
-      <div className=" w-full h-90 bg-white">
-            
+      <div className=" w-full h-90 bg-white text-black flex items-center justify-center text-center">
+        <div className=" p-10">
+          <h1 className=" text-2xl">
+            <span className=" text-primary">Pesquise</span> com precisão, decida
+            com dados.
+          </h1>
+          <p className="font-normal">
+            Coleta em campo, análise em tempo real — tudo em um só lugar.
+          </p>
+        </div>
+        
       </div>
     </section>
   );
